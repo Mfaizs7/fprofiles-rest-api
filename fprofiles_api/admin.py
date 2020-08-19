d3a77b6de6a24d5fc2a23eb619c7d72fe3a8b48c
faiz <muhammadfaizs90@gmail.com>
2020-08-19 20:23:50 +0530

    Added use profile feed A

--- fprofiles_api/admin.py
+++ fprofiles_api/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from fprofiles_api import models
 
 admin.site.register(models.UserProfile)
-
+admin.site.register(models.ProfileFeedItem)
 # Register your models here.
